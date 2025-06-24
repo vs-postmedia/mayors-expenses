9eae51b1eee160596b928353ceed4184231964dd
Nathan Griffiths <ngriffiths@postmedia.com>
2025-06-24 09:55:52 -0700

    copy edits

--- src/data/steps.js
+++ src/data/steps.js
@@ -1,6 +1,6 @@
 export default [
-    '#1: Expenses for the mayors office were <strong>$1.2 million</strong> in 2024, most of which went to salaries. But in Ken Sim’s administration, discretionary expenses like  <span class="green-01">meetings</span>, <span class="purple-01">transportation</span> and <span class="blue-01">services</span> were significantly higher than in either of the previous two administrations.<span class="scroll-more">Scroll for more...</span>',
-    '#2: In 2024, 96% of <span class="purple-01">transportation costs</span> were for a leased vehicle for staff use. Transportation costs for previous administrations were mainly for staff use of taxis, EVOs and transit.',
-    '#3: A lawsuit against the mayor’s office was the source of most of 2024’s <span class="blue-01">service expenses</span>.',
-    '#4: In 2024, <span class="green-01">meeting expenses</span> were more than double of any year since at least 2016, according to city data.',
+    '#Expenses for the mayors office were <strong>$1.2 million</strong> in 2024, most of which went to salaries. But in Ken Sim’s administration, discretionary expenses like  <span class="green-01">meetings</span>, <span class="purple-01">transportation</span> and <span class="blue-01">miscellaneous expenses</span> were significantly higher than in either of the previous two administrations.<span class="scroll-more">Scroll for more...</span>',
+    '#In 2024, 96% of <span class="purple-01">transportation costs</span> were from a leased vehicle for staff use. Transportation costs for previous administrations were almost entirely for taxis, EVOs and transit.',
+    'Most <span class="blue-01">miscellaneous</span> expenses were legal costs related to a code of conduct complaint, which was dismissed as unfounded in 2024.',
+    'In 2024, <span class="green-01">meeting expenses</span> were more than double of any year since at least 2016, according to city data.',
 ];
